cdd240e8c4298a411a8ec9b4bd68824cd5165541
Chen Jianzhang <jeza@vip.qq.com>
2018-04-11 09:39:22 +0800

    Update_20180411_更新了一些注释

--- Classes/CsvUtil.h
+++ Classes/CsvUtil.h
@@ -18,30 +18,30 @@ typedef std::map<std::string, StrDict> CsvMap;
 class CsvUtil
 {
 public:
-    /*
+    /**
     @brief   获取单实例
     @return  一个实例指针
     */
     static CsvUtil* getInstance();
 
-    /*
+    /**
     @brief   销毁当前实例
     */
     static void destroyInstance();
 
-    /*
+    /**
     @brief             载入一个文件到字典中
     @param FilePath    文件路径
     */
     virtual void addFileData(const std::string& FilePath);
 
-    /*
+    /**
     @brief              释放文件，在字典中删除该文件对应的数据
     @param FilePath     文件路径
     */
     virtual void releaseFile(const std::string& FilePath);
 
-    /*
+    /**
     @brief           在某个csv文件中读取某一行某一列的数据
 
     @param rRow      目标行数
@@ -54,21 +54,21 @@ public:
     virtual int getInt(const int& rRow, const int& rCol, const std::string &FilePath);
     virtual double getDouble(const int& rRow, const int& rCol, const std::string &FilePath);
 
-    /*
+    /**
     @brief           获取一整行的数据
     @param rRow      目标行数
     @param FilePath  文件路径
     */
     virtual StrVec getRowData(const int& rRow, const std::string& FilePath);
 
-    /*
+    /**
     @brief            获取该文件的行数和列数
     @param FilePath   文件路径
     @return           一个元组，第一个数是行数，第二个数是列数
     */
     virtual std::tuple<int, int> getFileRowColNum(const std::string &FilePath);
 
-    /*
+    /**
     @brief               在某个csv文件的某一列中寻找一个值，若寻找成功，返回对应的行数
     @param targetValue   目标值
     @param col           查找范围
@@ -78,7 +78,7 @@ public:
     virtual int findVal(const std::string &targetValue, const int &col, const std::string &FilePath);
 
 protected:
-    /*
+    /**
     @brief            获取某csv文件对应的StrDict
     @param FilePath   文件路径
     @return           文件对应的StrDict
--- Classes/HelpLayer.h
+++ Classes/HelpLayer.h
@@ -1,5 +1,6 @@
 ﻿/**
  * @brief 帮助界面图层
+ * @details 也就是帮助场景的第一个图层
  * @author 陈建彰
  * @date 20180410
  * */
@@ -23,17 +24,31 @@ public:
     {}
 
 protected:
+    /**
+     * @brief 初始化函数
+     * */
     bool init() override;
 
+    /**
+     * @brief 创建滚动页面的函数
+     * */
     virtual void createHelpPageView();
 
+    /**
+     * @brief 创建底部的函数
+     * @details 就是下方那个页码的生成，但并不负责那个动态页码的生成，动态页码的生成由回调函数进行操纵
+     * */
     void createBottom();
 
+    /**
+     * @brief 刚开始创造场景的时候调用该函数进行底部页面的快速初始化
+     * @details 由于动态页码显示的有点慢，所以写出这个函数出来，用于快速显示进去那个图层的对应页码
+     * */
     void createInitialBottomSprite();
 private:
     Sprite *bottomSprite;
-    int _iCurPageIndex = 0;
-    PageView *_pHelpPageView = nullptr;
+    int _iCurPageIndex = 0; //当前页码
+    PageView *_pHelpPageView = nullptr; //滚动页面
 };
 
 #endif //HELP_LAYER_H
