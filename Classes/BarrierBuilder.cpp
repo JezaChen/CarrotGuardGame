b9f5282cd25ddaac265a7ccdfd59bab6cb8df90c
twenty-twostrokes <36349121+twenty-twostrokes@users.noreply.github.com>
2018-04-17 21:24:23 +0800

    ====

--- Classes/BarrierBuilder.cpp
+++ Classes/BarrierBuilder.cpp
@@ -11,6 +11,7 @@ bool BarrierBuilder::init()
     bool bRet = false;
     do
     {
+		//确保创建障碍物函数实现；
         createBarriers();
         bRet = true;
     } while (0);
@@ -26,13 +27,15 @@ void BarrierBuilder::createBarriers()
 {
     //获取障碍物位置
     auto barriersPoses = MapUtil::getInstance()->getBarrierPoses();
+	//获取障碍物表格；
     CsvUtil *pCsvUtil = CsvUtil::getInstance();
     for (auto &iter : barriersPoses)
     {
+		//寻找障碍物；
         int iBarrierLine = pCsvUtil->findVal(iter.first, 1, BARRIERCSVFILE);
         Vec2 vPos = iter.second.origin; //获取坐标
         Size sSize = iter.second.size; //获取块大小
-        auto aBarrier = BarrierBase::create(iBarrierLine);
+        auto aBarrier = BarrierBase::create(iBarrierLine);//创建
         //障碍物锚点在中间
         aBarrier->setPosition(Vec2(vPos.x + sSize.width / 2, vPos.y + sSize.height / 2));
     }
--- Classes/BulletAnchor.h
+++ Classes/BulletAnchor.h
@@ -13,9 +13,21 @@ class BulletAnchor :public BulletBase
 	friend class TowerAnchor;
 
 protected:
+	/**
+	* @brief 锚子弹的基本动作
+	*/
 	void doAction();
+	/**
+	* @brief 锚子弹的运动
+	*/
 	void doMove()override;
+	/**
+	* @brief 保证子弹基类的成功初始化
+	*/
 	virtual bool init(const int &rId, VictimEntityBase *rVictimEntity);
+	/**
+	* @brief 创建锚的子弹
+	*/
 	static BulletAnchor * create(const int &rId, VictimEntityBase * rVictimEntity);
 
 };
