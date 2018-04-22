c2b7dddac8d8f74590c9b7699deeaab906f97cd9
twenty-twostrokes <36349121+twenty-twostrokes@users.noreply.github.com>
2018-04-22 10:46:57 +0800

    **

--- Classes/WelcomeScene.cpp
+++ Classes/WelcomeScene.cpp
@@ -1,3 +1,10 @@
+//
+//  TowerBlueStar.cpp
+//  CarrotFantasy
+//
+//  Created by 陈建彰 on 18-3-12.
+//  
+//
 #include "WelcomeScene.h"
 #include "SpritesLayer.h"
 #include "MenusLayer.h"
@@ -44,6 +51,8 @@ void WelcomeScene::onEnter()
     {
         addChild(_pMenuLayer);
     }
+
+	showVideo();
 }
 
 void WelcomeScene::showMenu()
@@ -52,3 +61,56 @@ void WelcomeScene::showMenu()
     addChild(_pMenuLayer);
     Config::getInstance()->setIsLoadSource(true);
 }
+
+
+//贪玩蓝月启动
+void WelcomeScene::showVideo()
+{
+	//获取可见屏幕大小；
+	Size size = Director::getInstance()->getVisibleSize();
+	//如果是安卓和ios；
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS)  
+	auto videoPlayer = cocos2d::experimental::ui::VideoPlayer::create();
+	//设置坐标；
+	videoPlayer->setPosition(Point(size.width / 2, size.height / 2));
+	//设置锚点；
+	videoPlayer->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+	//设置当前视频屏幕大小；
+	videoPlayer->setContentSize(Size(size.width, size.height));
+	this->addChild(videoPlayer);
+
+	if (videoPlayer)
+	{
+		videoPlayer->setFileName("twly.mp4");
+		videoPlayer->play();
+	}
+	videoPlayer->addEventListener(CC_CALLBACK_2(WelcomeScene::videoEventCallback, this));
+#endif  
+}
+
+void WelcomeScene::videoPlayOverCallback()
+{
+	//ToDo；
+}
+
+/**
+* @brief 视频播放的状态
+* @detai注意这里的代码，此处代码只有在android平台和Ios平台有效
+*/
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS)  
+void WelcomeScene::videoEventCallback(Ref* sender, cocos2d::experimental::ui::VideoPlayer::EventType eventType) {
+	switch (eventType) {
+	case cocos2d::experimental::ui::VideoPlayer::EventType::PLAYING:
+		break;
+	case cocos2d::experimental::ui::VideoPlayer::EventType::PAUSED:
+		break;
+	case cocos2d::experimental::ui::VideoPlayer::EventType::STOPPED:
+		break;
+	case cocos2d::experimental::ui::VideoPlayer::EventType::COMPLETED:
+		videoPlayOverCallback();
+		break;
+	default:
+		break;
+	}
+}
+#endif  
--- Classes/WelcomeScene.h
+++ Classes/WelcomeScene.h
@@ -22,6 +22,20 @@ protected:
     virtual void onEnter()override;
     virtual void showMenu();
 
+	/**
+	* @brief 视频播放完成的回调函数
+	*/
+	void videoPlayOverCallback();
+	/**
+	* @brief 视频显示函数
+	*/
+	void showVideo();
+	/**
+	* @brief 视频播放状态，只有在android和ios平台有效
+	*/
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS)  
+	void videoEventCallback(Ref* sender, cocos2d::experimental::ui::VideoPlayer::EventType eventType);
+#endif 
 private:
     Layer * _pSpritesLayer = nullptr;
     Layer *_pMenuLayer = nullptr;
